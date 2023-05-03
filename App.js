12ccdad81cba7c651edbf3166245c2f5cc420e32
khushboo khatwani <khushboo@vicdigit-technologies.com>
2023-05-03 12:51:13 +0200

    inception code practice

--- App.js
+++ App.js
@@ -1,7 +1,5 @@
-const heading = React.createElement("h1",{},"Namaste Everyone")
-const heading2 = React.createElement("h2",{},"Namaste Everyone")
-const container = React.createElement("div",{id:"container"},[heading,heading2])
-
+const heading = React.createElement("h1",{},"Hello From React ")
 
 const root = ReactDOM.createRoot(document.getElementById("root"))
-root.render(container);
+
+root.render(heading)
